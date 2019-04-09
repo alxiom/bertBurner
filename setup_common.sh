eaf4af755763dc7d56be0823d7964718dd2fd6bb
alexkim <hyoungseok.k@gmail.com>
2019-04-09 22:38:05 +0900

    [Bash] rename shell scripts

--- setup_common.sh
+++ setup_common.sh
@@ -2,13 +2,8 @@
 
 projectPath=${PWD}
 
-cd bertScala
-sbt update compile
-cd target/scala-2.12/classes
-javah -cp /usr/local/Cellar/scala/2.12.8/libexec/lib/scala-library.jar:. EvalJNI
-mv EvalJNI.h ${projectPath}/bertCpp
-
 cd ${projectPath}/bertCpp
+rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH=${projectPath}/libtorch ..
@@ -19,8 +14,8 @@ cd ${projectPath}/bertScala
 sbt universal:packageBin
 mv ${projectPath}/bertScala/target/universal/bertburner-0.1.0.zip ${projectPath}
 
-rm -rf burner
 cd ${projectPath}
+rm -rf burner
 unzip bertburner-0.1.0.zip
 rm bertburner-0.1.0.zip
 mv bertburner-0.1.0 burner
